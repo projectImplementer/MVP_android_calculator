fecd3f171a2fb8e956e78cb5128c6544736fed4b
projectImplementer <bogdanm.demetrescu@gmail.com>
2019-07-21 12:09:38 +0300

    added digit interaction effect, changed layout, added scrollbar, landscape added and resized for 5.0inches screen

--- app/src/main/java/designpattern_mvp/calculator/main/SecondActivity.java
+++ app/src/main/java/designpattern_mvp/calculator/main/SecondActivity.java
@@ -4,21 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import designpattern_mvp.calculator.R;
 
 public class SecondActivity extends AppCompatActivity {
     private static final int MAIN_ACTIVITY_REQUEST_CODE = 0;
 
+    @BindView (R.id.showResults) TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        ButterKnife.bind(this);
+
+        textView.setMovementMethod(new ScrollingMovementMethod());
 
         String savedExtra = getIntent().getStringExtra("resultKey");
-        TextView text = findViewById(R.id.showResults);
-        text.setText(savedExtra);
+        textView.setText(savedExtra);
     }
 
 }
